c3812bdea5c4593032c42d14c23c852386a28d07
hunterKiller922 <85689559+hunterKiller922@users.noreply.github.com>
2024-11-28 16:12:02 +0700

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -1,5 +1,5 @@
 # Создаем пустой список для заголовков
-titles = []
+title = []
 
 # Запрашиваем у пользователя три заголовка
 title1 = input("Введите первый заголовок: ")
@@ -7,11 +7,41 @@ title2 = input("Введите второй заголовок: ")
 title3 = input("Введите третий заголовок: ")
 
 # Добавляем заголовки в список
-titles.append(title1)
-titles.append(title2)
-titles.append(title3)
+title.append(title1)
+title.append(title2)
+title.append(title3)
 
 # Выводим все заголовки
-print("Заголовки заметки:")
-for title in titles:
-    print(title)
+for titles in title:
+    print(titles, end=', ')
+
+print(end='\n---------------------\n')
+
+# Создаем пустой список для заголовков
+title = []
+
+# Запрашиваем у пользователя три заголовка
+title1 = input("Введите первый заголовок: ")
+title2 = input("Введите второй заголовок: ")
+title3 = input("Введите третий заголовок: ")
+
+# Добавляем заголовки в список
+title.extend([title1 , title2 , title3])
+
+
+# Выводим все заголовки
+
+for titles in title:
+    print(titles, end=', ')
+
+print(end='\n---------------------\n')
+
+title1 = input("Введите первый заголовок: ")
+title2 = input("Введите второй заголовок: ")
+title3 = input("Введите третий заголовок: ")
+title = [title1 , title2 , title3]
+
+for titles in title:
+           print(titles,end=', ')
+
+print(end='\n---------------------\n')
